4e82d095c4971dc3b6a1f387009821a9ded4384e
Ksenia Sidorova <nekfi@mail.ru>
2020-10-09 20:21:01 +0300

    Правки

--- src/js/__tests__/homework__1.test.js
+++ src/js/__tests__/homework__1.test.js
@@ -1,14 +1,6 @@
 import GameSavingLoader from '../homework__1';
 
 test('load', () => {
-    const data = { "id": 9,
-                   "created": 1546300800,
-                   "userInfo": { 
-                       "id":1,
-                       "name":"Hitman",
-                       "level":10,
-                       "points":2000 
-                    } 
-                } 
+    const data = {"id": 9, "created": 1546300800, "userInfo": { "id":1, "name":"Hitman", "level":10, "points":2000 }} 
     return expect(GameSavingLoader.load()).resolves.toEqual(data);
 });
--- src/js/homework__2.js
+++ src/js/homework__2.js
@@ -4,7 +4,7 @@ import read from './reader';
 export default class GameSavingLoader {
 
     static load() {
-        (async () => {
+        return (async () => {
             try {
                 const data = await read();
                 const value = await json(data);
