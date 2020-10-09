c299f47387e2a68b4a0a5f9c9a2b864aa8ace7ec
Ksenia Sidorova <nekfi@mail.ru>
2020-10-09 03:25:43 +0300

    Правки 1 задание

--- src/js/__tests__/homework__1.test.js
+++ src/js/__tests__/homework__1.test.js
@@ -1,5 +1,14 @@
 import GameSavingLoader from '../homework__1';
 
 test('load', () => {
-    return expect(GameSavingLoader.load().resolves.toBe())
+    const data = { "id": 9,
+                   "created": 1546300800,
+                   "userInfo": { 
+                       "id":1,
+                       "name":"Hitman",
+                       "level":10,
+                       "points":2000 
+                    } 
+                } 
+    return expect(GameSavingLoader.load().resolves.toBe(data))
 });
